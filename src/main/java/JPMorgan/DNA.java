c4e0e5b25d94eefbb1c17aa571f6a74f3ed4ff4e
Boryana1 <boryana.dimova1@gmail.com>
2022-07-06 10:51:24 -0500

    Completed.

---
+++ src/main/java/JPMorgan/DNA.java
@@ -0,0 +1,37 @@
+package JPMorgan;
+
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class DNA {
+
+    public static String dnaCompliment(String s) {
+        //write your code here
+        String[] splitDNA = s.split("");
+        String[] reverseDNA = new String[splitDNA.length];
+        for (int i = splitDNA.length - 1; i >= 0; i--) {
+            String letter = splitDNA[i];
+            reverseDNA[reverseDNA.length - 1 - i] = letter;
+        }
+//        System.out.println(Arrays.toString(reverseDNA));
+        for (int i = 0; i < reverseDNA.length; i++) {
+            if (reverseDNA[i].equals("A")) {
+                reverseDNA[i] = "T";
+            } else if (reverseDNA[i].equals("T")) {
+                reverseDNA[i] = "A";
+            } else if (reverseDNA[i].equals("G")) {
+                reverseDNA[i] = "C";
+            } else if (reverseDNA[i].equals("C")) {
+                reverseDNA[i] = "G";
+            }
+        }
+        return String.join("", reverseDNA);
+    }
+
+    static String DNA = "GTCAG";
+
+    public static void main(String[] args) {
+        System.out.println(dnaCompliment(DNA));
+    }
+}
