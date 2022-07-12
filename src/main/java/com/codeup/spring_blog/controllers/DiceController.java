410467d5143e5a5ada26150547f8bc03c5e4d459
Boryana1 <boryana.dimova1@gmail.com>
2022-07-11 21:06:18 -0500

    Completed.

---
+++ src/main/java/com/codeup/spring_blog/controllers/DiceController.java
@@ -0,0 +1,32 @@
+package com.codeup.spring_blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class DiceController {
+
+    @GetMapping("/roll-dice/")
+    public String diceStart(){
+        return "rollDiceView";
+    }
+
+    @GetMapping("/roll-dice/{number}")
+    public String rollDice(@PathVariable int number, Model model) {
+        int randomNum = (int)Math.floor(Math.random()*(6-1+1)+1);
+
+        String message = "";
+
+        if(randomNum == number){
+            message = "You guessed correctly";
+        }else{
+            message = "That is a wrong number";
+        }
+        System.out.println("What did we get? Random number = " + randomNum + ";" + "userNumber = " + number);
+        System.out.println("message = " + message);
+        model.addAttribute("number", number);
+        return "rollDiceView";
+    }
+}
