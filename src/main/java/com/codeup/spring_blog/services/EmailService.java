38f024ec992b74945c8bc0da90e67dc76b76bf2d
Boryana1 <boryana.dimova1@gmail.com>
2022-09-13 18:56:09 -0500

    Created prepareAndSend()

--- src/main/java/com/codeup/spring_blog/services/EmailService.java
+++ src/main/java/com/codeup/spring_blog/services/EmailService.java
@@ -17,13 +17,13 @@ public class EmailService {
     @Value("${spring.mail.from}")
     private String from;
 
-//    public void prepareAndSend(Ad ad, String subject, String body) {
-//        SimpleMailMessage msg = new SimpleMailMessage();
-//        msg.setFrom(from);
-//        msg.setTo(ad.getOwner().getEmail());
-//        msg.setSubject(subject);
-//        msg.setText(body);
-//
+    public void prepareAndSend(Ad ad, String subject, String body) {
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setFrom(from);
+        msg.setTo(ad.getOwner().getEmail());
+        msg.setSubject(subject);
+        msg.setText(body);
+
 //        try {
 //            this.emailSender.send(msg);
 //        } catch (MailException ex) {
